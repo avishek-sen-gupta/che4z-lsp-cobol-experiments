3f49a5271c25af46589b263058535a74484e03c6
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-05-13 16:04:33 +0530

    Printing out original text

--- server/engine/src/main/java/org/eclipse/lsp/cobol/cli/CobolContextAugmentedTreeNode.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/cli/CobolContextAugmentedTreeNode.java
@@ -15,7 +15,12 @@
 package org.eclipse.lsp.cobol.cli;
 
 import hu.webarticum.treeprinter.SimpleTreeNode;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.misc.Interval;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
 import org.eclipse.lsp.cobol.common.model.tree.Node;
 import org.eclipse.lsp.cobol.core.CobolParser;
 
@@ -36,7 +41,17 @@ public class CobolContextAugmentedTreeNode extends SimpleTreeNode {
     }
 
     private String withType(ParseTree astNode) {
-        String text = astNode.getText().length() > 50 ? astNode.getText().substring(0, 50) + " ... (truncated)" : astNode.getText();
+//        ParserRuleContext context = (ParserRuleContext) astNode;
+//        Token start = context.start;
+//        Token stop = context.stop;
+        Token startToken = (astNode instanceof TerminalNode) ? ((TerminalNode)(astNode)).getSymbol() : ((ParserRuleContext)(astNode)).start;
+        Token stopToken = (astNode instanceof TerminalNode) ? ((TerminalNode)(astNode)).getSymbol() : ((ParserRuleContext)(astNode)).stop;
+
+        CharStream cs = startToken.getInputStream();
+        int stopIndex = stopToken != null ? stopToken.getStopIndex() : -1;
+        String originalText = stopIndex >= startToken.getStartIndex() ? cs.getText(new Interval(startToken.getStartIndex(), stopIndex)) : "<NULL>";
+//        String text = astNode.getText().length() > 50 ? astNode.getText().substring(0, 50) + " ... (truncated)" : astNode.getText();
+        String text = originalText.length() > 50 ? originalText.substring(0, 50) + " ... (truncated)" : originalText;
         return astNode.getClass().getSimpleName() + " / " + text;
     }
 }
