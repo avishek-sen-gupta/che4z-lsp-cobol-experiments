5b4e458f6f8cb7eec35cecd30549f0c1b8ccaa95
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-05-26 21:10:54 +0530

    Testing the agent

---
+++ server/poc-ai/src/main/java/org/poc/ai/Advisor.java
@@ -0,0 +1,45 @@
+package org.poc.ai;
+
+import com.azure.ai.openai.OpenAIClient;
+import com.azure.ai.openai.OpenAIClientBuilder;
+import com.azure.ai.openai.models.*;
+import com.azure.core.credential.AzureKeyCredential;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Advisor {
+    private final String azureOpenaiApiKey;
+    private final String azureOpenaiEndpoint;
+    private final OpenAIClient client;
+    private String deploymentOrModelId = "gpt35";
+
+    public Advisor(String azureOpenaiApiKey, String azureOpenaiEndpoint) {
+        this.azureOpenaiApiKey = azureOpenaiApiKey;
+        this.azureOpenaiEndpoint = azureOpenaiEndpoint;
+        client = new OpenAIClientBuilder()
+                .endpoint(azureOpenaiEndpoint)
+                .credential(new AzureKeyCredential(azureOpenaiApiKey))
+                .buildClient();
+    }
+
+    public List<String> advise(String prompt) {
+        List<ChatMessage> prompt2 = new ArrayList<>();
+        prompt2.add(new ChatMessage(ChatRole.USER, prompt));
+
+        ChatCompletions completions = client.getChatCompletions(deploymentOrModelId, new ChatCompletionsOptions(prompt2));
+
+        System.out.printf("Model ID=%s is created at %s.%n", completions.getId(), completions.getCreatedAt());
+        for (ChatChoice choice : completions.getChoices()) {
+            System.out.printf("Index: %d, Text: %s.%n", choice.getIndex(), choice.getMessage().getContent());
+        }
+
+        List<String> responses = completions.getChoices().stream().map(c -> c.getMessage().getContent()).toList();
+
+        CompletionsUsage usage = completions.getUsage();
+        System.out.printf("Usage: number of prompt token is %d, "
+                        + "number of completion token is %d, and number of total tokens in request and response is %d.%n",
+                usage.getPromptTokens(), usage.getCompletionTokens(), usage.getTotalTokens());
+        return responses;
+    }
+}
--- server/poc-ai/src/main/java/org/poc/ai/OpenAiTest.java
+++ server/poc-ai/src/main/java/org/poc/ai/OpenAiTest.java
@@ -1,7 +1,12 @@
 package org.poc.ai;
 
+import java.util.List;
+
 public class OpenAiTest {
     public static void main(String[] args) {
         System.out.println("Working");
+        Advisor advisor = new Advisor(System.getenv("AZURE_OPENAI_API_KEY"), System.getenv("AZURE_OPENAI_ENDPOINT"));
+        List<String> responses = advisor.advise("When was Microsoft founded?");
+        responses.forEach(System.out::println);
     }
 }
