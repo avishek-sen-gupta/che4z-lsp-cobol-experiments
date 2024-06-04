5f49622f27f1e68af69c01dca49591290c54aa25
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-06-04 23:39:53 +0530

    Adding StackFrames operations

--- server/poc-common/src/main/java/poc/common/flowchart/StackFrames.java
+++ server/poc-common/src/main/java/poc/common/flowchart/StackFrames.java
@@ -1,4 +1,22 @@
 package poc.common.flowchart;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StackFrames {
+    List<ChartNode> frames = new ArrayList<>();
+
+    public StackFrames() {
+        this(new ArrayList<>());
+    }
+
+    public StackFrames(List<ChartNode> frames) {
+        this.frames = frames;
+    }
+
+    public StackFrames add(ChartNode frame) {
+        List<ChartNode> shallowCopy = new ArrayList<>(frames);
+        shallowCopy.add(frame);
+        return new StackFrames(shallowCopy);
+    }
 }
