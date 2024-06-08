282d5ed667bad89aa446dbfc2cbacd865d5f5b7f
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-06-08 22:53:34 +0530

    Getting data layouts from linkage section also

--- server/poc-flow/src/main/java/org/poc/vm/DataStructureBuilder.java
+++ server/poc-flow/src/main/java/org/poc/vm/DataStructureBuilder.java
@@ -9,6 +9,7 @@ import poc.common.flowchart.IDataStructureBuilder;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 public class DataStructureBuilder implements IDataStructureBuilder {
     private final CobolEntityNavigator navigator;
@@ -20,14 +21,33 @@ public class DataStructureBuilder implements IDataStructureBuilder {
 
     @Override
     public DataStructure build() {
+        zerothStructure = new DataStructure(0);
         ParseTree dataDivision = navigator.dataDivisionBodyRoot();
         CobolParser.DataDivisionContext dataDivisionBody = (CobolParser.DataDivisionContext) dataDivision;
         Optional<CobolParser.DataDivisionSectionContext> maybeWorkingStorage = dataDivisionBody.dataDivisionSection().stream().filter(s -> s.workingStorageSection() != null).findFirst();
         CobolParser.WorkingStorageSectionContext workingStorageSection = maybeWorkingStorage.get().workingStorageSection();
-        List<CobolParser.DataDescriptionEntryForWorkingStorageSectionContext> dataLayouts = workingStorageSection.dataDescriptionEntryForWorkingStorageSection();
-        zerothStructure = new DataStructure(0);
+        List<CobolParser.DataDescriptionEntryForWorkingStorageSectionContext> workingStorageDataLayouts = workingStorageSection.dataDescriptionEntryForWorkingStorageSection();
+        extractFrom2(workingStorageDataLayouts, zerothStructure, this::wsData);
+
+        Optional<CobolParser.DataDivisionSectionContext> maybeLinkageSection = dataDivisionBody.dataDivisionSection().stream().filter(s -> s.linkageSection() != null).findFirst();
+        CobolParser.LinkageSectionContext linkageSection = maybeLinkageSection.get().linkageSection();
+        List<CobolParser.DataDescriptionEntryForWorkingStorageAndLinkageSectionContext> linkageSectionDataLayouts = linkageSection.dataDescriptionEntryForWorkingStorageAndLinkageSection();
+        extractFrom2(linkageSectionDataLayouts, zerothStructure, this::linkageData);
+
+        return zerothStructure;
+    }
+
+    private CobolParser.DataDescriptionEntryContext wsData(CobolParser.DataDescriptionEntryForWorkingStorageSectionContext e) {
+        return e.dataDescriptionEntryForWorkingStorageAndLinkageSection().dataDescriptionEntry();
+    }
+
+    private CobolParser.DataDescriptionEntryContext linkageData(CobolParser.DataDescriptionEntryForWorkingStorageAndLinkageSectionContext e) {
+        return e.dataDescriptionEntry();
+    }
+
+    private void extractFrom(List<CobolParser.DataDescriptionEntryForWorkingStorageSectionContext> dataLayouts) {
         int currentLevel = 0;
-        DataStructure dataStructure = null;
+        DataStructure dataStructure = zerothStructure;
         for (CobolParser.DataDescriptionEntryForWorkingStorageSectionContext dataDescriptionEntry : dataLayouts) {
             CobolParser.DataDescriptionEntryContext dataDescription = dataDescriptionEntry.dataDescriptionEntryForWorkingStorageAndLinkageSection().dataDescriptionEntry();
             if (dataDescription.dataDescriptionEntryFormat1() != null) {
@@ -35,7 +55,7 @@ public class DataStructureBuilder implements IDataStructureBuilder {
                 int entryLevel = Integer.valueOf(format1.levelNumber().LEVEL_NUMBER().getSymbol().getText());
                 if (currentLevel == 0) {
                     if (entryLevel != 1) throw new RuntimeException("Top Level entry must be 01");
-                    dataStructure = zerothStructure.addChild(new DataStructure(format1));
+                    dataStructure = dataStructure.addChild(new DataStructure(format1));
                 } else if (entryLevel == currentLevel) {
                     dataStructure = dataStructure.addPeer(new DataStructure(format1));
                 } else if (entryLevel > currentLevel) {
@@ -49,6 +69,33 @@ public class DataStructureBuilder implements IDataStructureBuilder {
             }
             currentLevel = dataStructure.level();
         }
-        return zerothStructure;
+    }
+
+    private static <T> DataStructure extractFrom2(List<T> dataLayouts, DataStructure root, Function<T, CobolParser.DataDescriptionEntryContext> retriever) {
+        int currentLevel = 0;
+        DataStructure dataStructure = root;
+        for (T dataDescriptionEntry : dataLayouts) {
+            CobolParser.DataDescriptionEntryContext dataDescription = retriever.apply(dataDescriptionEntry);
+            if (dataDescription.dataDescriptionEntryFormat1() != null) {
+                CobolParser.DataDescriptionEntryFormat1Context format1 = dataDescription.dataDescriptionEntryFormat1();
+                int entryLevel = Integer.valueOf(format1.levelNumber().LEVEL_NUMBER().getSymbol().getText());
+                if (currentLevel == 0) {
+                    if (entryLevel != 1) throw new RuntimeException("Top Level entry must be 01");
+                    dataStructure = dataStructure.addChild(new DataStructure(format1));
+                } else if (entryLevel == currentLevel) {
+                    dataStructure = dataStructure.addPeer(new DataStructure(format1));
+                } else if (entryLevel > currentLevel) {
+                    dataStructure = dataStructure.addChild(new DataStructure(format1));
+                } else {
+                    dataStructure = dataStructure.parent(entryLevel - 1).addChild(new DataStructure(format1));
+                }
+            } else if (dataDescription.dataDescriptionEntryFormat3() != null) {
+                CobolParser.DataDescriptionEntryFormat3Context conditionalFormat = dataDescription.dataDescriptionEntryFormat3();
+                dataStructure = dataStructure.addConditionalVariable(new ConditionalDataStructure(conditionalFormat));
+            }
+            currentLevel = dataStructure.level();
+        }
+
+        return root;
     }
 }
